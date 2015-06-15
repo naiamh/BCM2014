1ac7bb1f2d3c81da3bb4e630100a84ee2f6c8bcb
Naia Morueta Holme <morueta-holme@berkeley.edu>
2015-06-15 15:32:41 -0700

    Updated functions
    Erased irrelevant functions and updated multiyrave() so user can specify
    temporary directory (tdir) and whether input climate data follows water
    years or not (calendar year instead)

--- BCM2014_summary_functions.r
+++ BCM2014_summary_functions.r
@@ -5,7 +5,7 @@
 # Function to get the file names of raster of interest to compute summaries
 # can be used when only particular months of each year are wanted
 # Multiple vars can be chosen (only meaningful for e.g. tmn and tmx!)
-getBCMfilenames = function(models, vars, years, months) {
+getBCMfilenames = function(models, vars, years, months,cdir) {
   require(raster)
     
   cmonths <- as.character(months)
@@ -13,7 +13,7 @@ getBCMfilenames = function(models, vars, years, months) {
   
   gr = expand.grid(models,vars,years,cmonths)
   res = apply(gr,1,function(x) {
-    paste('CA_BCM_',x[1],'_Monthly_',x[2],'_',x[3],'_',x[4],'.Rdata',sep='')
+    paste(cdir,'CA_BCM_',x[1],'_Monthly_',x[2],'_',x[3],'_',x[4],'.Rdata',sep='')
   })
   
   return(res)
@@ -23,65 +23,42 @@ getBCMfilenames = function(models, vars, years, months) {
 
 # Function to get the mean across BCM .rdata layers. 
 # A chunksize can be set to limit the number of layers read into memory at a time.
-# Returns a raster layer
-rMeanFromFiles = function(fv, chunksize=2) {
+# Returns a raster layer. tdir sets the temporary directory
+rMeanFromFiles = function(fv, chunksize=2, tdir) {
   require(raster)
+  rasterOptions(tmpdir=tdir)
   
   n = 1:length(fv)
   subs = split(n, ceiling(seq_along(n)/chunksize))
   for(i in 1:length(subs)) {
+    print(paste(i, 'of', length(subs)))
     slist = lapply(fv[subs[[i]]], function(x) {readRDS(x)[[1]]})
     tmpres = mean(stack(slist))
-    res = ifelse(i == 1, tmpres, mean(res,tmpres)) 
-  }   
+    if(i==1) {
+      res = tmpres
+    } else {
+      res = mean(res, tmpres)
+    }
+  }
+  rm(tmpres)
   return(res)
 }
 
-water.year2 <- function(year) {
-  wyear = c(rep(year-1,3),rep(year,8))
-  wmonth = c(8:12,1:8)
-  
-  wyear <- year
-  wmonth <- month
-  wyear[month>9] <- year+1
-  wmonth <- month+3
-  wmonth[wmonth>12] <- wmonth-12
-  wyear.month <- wyear + (wmonth-1)/12
-  return(data.frame(wyear,wmonth,wyear.month))
-}
 
-multiwyrave = function(wyrstart, noyrs=30, m, v) {
-  yrs = (wyrstart+1):(wyrstart+noyrs)
-  l1 = getBCMfilenames(models=m, vars=v, years=yrs, months=1:12)
-  head = getBCMfilenames(models=m,vars=v,years=yrs[1]-1, months=10:12)
-  cutoff= getBCMfilenames(models=m,vars=v,years=yrs[length(yrs)], months=10:12)
-  wf = c(head, l1[!l1%in%cutoff])
-  
-  p1 = rMeanFromFiles(fv=wf, chunksize=12)
+
+multiwyrave = function(wyrstart, noyrs=30, m, v, cdir, tdir, wyrIn) {
+  print(paste(wyrstart,m,v))
+  yrs = (wyrstart):(wyrstart+noyrs-1)
+  l1 = getBCMfilenames(models=m, vars=v, years=yrs, cdir=cdir, months=1:12)
+  if(wyrIn == F) {
+    head = getBCMfilenames(models=m,vars=v,years=yrs[1]-1, cdir=cdir, months=10:12)
+    cutoff= getBCMfilenames(models=m,vars=v,years=yrs[length(yrs)], cdir=cdir, months=10:12)
+    wf = c(head, l1[!l1%in%cutoff])
+  } else {
+    wf = l1
+  }
+    
+  p1 = rMeanFromFiles(fv=wf, chunksize=12, tdir=tdir)
+  print(Sys.time())
   return(p1)
 } 
-
-
-
-# # Small function to calculate the mean across a given time series
-# # reads in the relevant .rdata files and returns a summary raster
-# MTmean = function(model='HST',var='ppt',mstart=1,mend=5,ystart=1920,yend=1920) {
-#   require(raster)
-#   
-#   months <- as.character(mstart:mend)
-#   months[nchar(months)==1] <- paste('0',months[nchar(months)==1],sep='')
-#   
-#   for(year in ystart:yend) {
-#     tmp_list = list()
-#     for(m in 1:length(months)) {
-#       tmp_list[[m]] = readRDS(paste('CA_BCM_',model,'_Monthly_',var,'_',year,'_',months[m],'.Rdata',sep=''))[[1]]
-#     }
-#     tmp_mean = mean(stack(tmp_list))
-#   }
-#   if(year==ystart | ystart==yend) {
-#     res=tmp_mean
-#   } else {
-#     res=mean(res,tmp_mean)
-#   }  
-#   return(res)
-# }
