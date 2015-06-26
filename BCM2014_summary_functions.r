e635a3d3898fedd12a444b1afedd739a97a64532
Naia Morueta Holme <morueta-holme@berkeley.edu>
2015-06-26 13:34:33 -0600

    Renamed and added a few functions
    Multiple year averages from monthlies and from one year summaries.
    monthlytowyrave also can handle subsets of months

--- BCM2014_summary_functions.r
+++ BCM2014_summary_functions.r
@@ -10,15 +10,14 @@ getBCMfilenames = function(models, vars, years, months,cdir) {
     
   cmonths <- as.character(months)
   cmonths[nchar(cmonths)==1] <- paste('0',cmonths[nchar(cmonths)==1],sep='')
-  
-  gr = expand.grid(models,vars,years,cmonths)
-  res = apply(gr,1,function(x) {
+
+  gr <- expand.grid(models,vars,years,cmonths)
+  res <- apply(gr,1,function(x) {
     paste(cdir,'CA_BCM_',x[1],'_Monthly_',x[2],'_',x[3],'_',x[4],'.Rdata',sep='')
   })
   
   return(res)
 }
-
 
 
 # Function to get the mean across BCM .rdata layers. 
@@ -28,16 +27,16 @@ rMeanFromFiles = function(fv, chunksize=2, tdir) {
   require(raster)
   rasterOptions(tmpdir=tdir)
   
-  n = 1:length(fv)
-  subs = split(n, ceiling(seq_along(n)/chunksize))
+  n <- 1:length(fv)
+  subs <- split(n, ceiling(seq_along(n)/chunksize))
   for(i in 1:length(subs)) {
     print(paste(i, 'of', length(subs)))
-    slist = lapply(fv[subs[[i]]], function(x) {readRDS(x)[[1]]})
-    tmpres = mean(stack(slist))
+    slist <- lapply(fv[subs[[i]]], function(x) {readRDS(x)[[1]]})
+    tmpres <- mean(stack(slist))
     if(i==1) {
-      res = tmpres
+      res <- tmpres
     } else {
-      res = mean(res, tmpres)
+      res <- mean(res, tmpres)
     }
   }
   rm(tmpres)
@@ -45,20 +44,70 @@ rMeanFromFiles = function(fv, chunksize=2, tdir) {
 }
 
 
-
-multiwyrave = function(wyrstart, noyrs=30, m, v, cdir, tdir, wyrIn) {
+# Function to calculate average across one or multiple years based on monthly .Rdata files
+# recommended for just 1 year (can use 1 year averages for faster multiple year averages)
+monthlytowyrave = function(wyrstart, noyrs=30, m, v, cdir, tdir, wyrIn) {
   print(paste(wyrstart,m,v))
-  yrs = (wyrstart):(wyrstart+noyrs-1)
-  l1 = getBCMfilenames(models=m, vars=v, years=yrs, cdir=cdir, months=1:12)
+  yrs <- (wyrstart):(wyrstart+noyrs-1)
+  l1 <- getBCMfilenames(models=m, vars=v, years=yrs, cdir=cdir, months=1:12)
   if(wyrIn == F) {
-    head = getBCMfilenames(models=m,vars=v,years=yrs[1]-1, cdir=cdir, months=10:12)
-    cutoff= getBCMfilenames(models=m,vars=v,years=yrs[length(yrs)], cdir=cdir, months=10:12)
-    wf = c(head, l1[!l1%in%cutoff])
+    head <- getBCMfilenames(models=m,vars=v,years=yrs[1]-1, cdir=cdir, months=10:12)
+    cutoff <- getBCMfilenames(models=m,vars=v,years=yrs[length(yrs)], cdir=cdir, months=10:12)
+    wf <- c(head, l1[!l1%in%cutoff])
   } else {
-    wf = l1
+    wf <- l1
   }
     
-  p1 = rMeanFromFiles(fv=wf, chunksize=12, tdir=tdir)
+  p1 <- rMeanFromFiles(fv=wf, chunksize=12, tdir=tdir)
   print(Sys.time())
+  return(p1)
+} 
+
+# Function to calculate average across one or multiple years based on monthly .Rdata files
+# recommended for just 1 year (can use 1 year averages for faster multiple year averages)
+# submonths can be used to specify particular months of the year to average across.
+# For submonths, if some months are from the previous calendar year, they must me specified as negative number of
+# months since new years (i.e. -1 for December, -2 for November, etc.)
+monthlytowyrave = function(wyrstart, noyrs=30, m, v, cdir, tdir, wyrIn, submonths='all') {
+  print(paste(wyrstart,m,v))
+  yrs <- (wyrstart):(wyrstart+noyrs-1)
+    
+  if(submonths[1]!= 'all') { 
+    head <- c()
+    head <- getBCMfilenames(models=m, vars=v, years=yrs-1, cdir=cdir, months=13-(-submonths[which(submonths<0)]))
+    l1 <- getBCMfilenames(models=m, vars=v, years=yrs, cdir=cdir, months=submonths[which(submonths>0)])
+    wf <- c(head,l1)
+    
+  } else {
+    l1 <- getBCMfilenames(models=m, vars=v, years=yrs, cdir=cdir, months=1:12)
+    if(wyrIn == F) {
+      head <- getBCMfilenames(models=m,vars=v,years=yrs[1]-1, cdir=cdir, months=10:12)
+      cutoff <- getBCMfilenames(models=m,vars=v,years=yrs[length(yrs)], cdir=cdir, months=10:12)
+      wf <- c(head, l1[!l1%in%cutoff])
+    } else {
+      wf <- l1
+    } 
+  }
+  
+  p1 <- rMeanFromFiles(fv=wf, chunksize=12, tdir=tdir)
+  print(Sys.time())
+  return(p1)
+} 
+
+# Function to get average across multiple years based on one year averages
+# v can also be jja or djf
+onewyrtomulti = function(wyrstart, noyrs=30, m=mod, v=var, cdir=idir, tdir=tdir) {
+  require(raster)
+  rasterOptions(tmpdir=tdir)
+  
+  print(paste(wyrstart,m,v))
+  
+  yrs = (wyrstart):(wyrstart+noyrs-1)
+  
+  # files to average across
+  fns <- sapply(yrs, function(x) {paste(cdir,'BCM2014_',v,x,'_wy_ave_',m,'.Rdata',sep='')})
+  
+  p1 <- rMeanFromFiles(fv=fns, chunksize=10, tdir=tdir)
+  
   return(p1)
 } 
